90803a95aed47a24e4165201c7de158006549703
Jake Beecham <60218222+JakeBeecham@users.noreply.github.com>
2021-02-24 11:27:24 +0000

    OODWeek5Labsheet3 Exercise3 Complete.

--- OOD_Labsheet3/MainWindow.xaml.cs
+++ OOD_Labsheet3/MainWindow.xaml.cs
@@ -31,8 +31,11 @@ namespace OOD_Labsheet3
         //Ex 1 - Customer Names
         private void btnQueryEx1_Click(object sender, RoutedEventArgs e)
         {
+            //Query
             //var query = from c in db.Customers
             //            select c.CompanyName;
+
+            //Lambda
             var query = db.Customers.Select(c => c.CompanyName);
 
             lbxCustomersEx1.ItemsSource = query.ToList();
@@ -43,11 +46,48 @@ namespace OOD_Labsheet3
         //Ex 2 - Customer Objects
         private void btnQueryEx2_Click(object sender, RoutedEventArgs e)
         {
+            //Query
             //var query = from c in db.Customers
             //            select c;
+
+            //Lambda
             var query = db.Customers.Select(c => c);
 
             dgCustomersEx2.ItemsSource = query.ToList();
+        }
+        #endregion
+
+        #region Ex3
+        //Ex 3 - Order Information
+        private void btnQueryEx3_Click(object sender, RoutedEventArgs e)
+        {
+            //Query
+            //var query = from o in db.Orders
+            //            where o.Customer.City.Equals("London") 
+            //            || o.Customer.City.Equals("Paris") 
+            //            || o.Customer.Country.Equals("USA")
+            //            orderby o.Customer.CompanyName
+            //            select new
+            //            {
+            //                CustomerName = o.Customer.CompanyName,
+            //                City = o.Customer.City,
+            //                Address = o.ShipAddress
+            //            };
+
+            //Lambda
+            var query = db.Orders
+                .Where(o => o.Customer.City.Equals("London")
+                        || o.Customer.City.Equals("Paris")
+                        || o.Customer.Country.Equals("USA"))
+                .OrderBy(o => o.Customer.CompanyName)
+                .Select(o => new 
+                { 
+                    CustomerName = o.Customer.CompanyName, 
+                    City = o.Customer.City, 
+                    Address = o.ShipAddress 
+                });
+
+            dgCustomersEx3.ItemsSource = query.ToList().Distinct();
         }
         #endregion
     }
